31d6333cda9e5faebe18db0e05bc145b54a11f63
frankfang <frankfang1990@gmail.com>
2017-03-24 17:42:21 +0800

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
 
 const config = {
   entry: {
-    'components/tabs': './components/tabs/index.js'
+    'wheels': './src/index'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
+    library: "wheels", 
+    libraryTarget: "umd",
   },
   module: {
     rules: [
       {test: /\.(js|jsx)$/, use: 'babel-loader'}
     ]
-  }
+  },
+  plugins: [
+    //new webpack.optimize.UglifyJsPlugin(),
+    //new HtmlWebpackPlugin({
+      //template: './src/demo.html',
+      //filename: 'demo.html'
+    //})
+  ]
 };
 
 module.exports = config;
