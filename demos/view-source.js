58bb9f43272b507d66b65c194fe2154eeb433f63
frankfang <frankfang1990@gmail.com>
2017-05-07 23:25:24 +0800

    update position

--- demos/view-source.js
+++ demos/view-source.js
@@ -3,11 +3,12 @@
   let name = parts[parts.length - 1 - 1]
   let a = dom.create(`<a id="viewSource" href="https://github.com/FrankFang/wheels#如何阅读源代码">查看源码</a>`)
   a.style.position = 'fixed'
-  a.style.top = '.2em'
+  a.style.bottom = '.2em'
   a.style.right = '.2em'
   a.style.background = 'black'
   a.style.border = '1px solid white'
   a.style.color = 'white'
   a.style.padding = '.2em'
+  a.style.textDecoration = 'none'
   document.body.appendChild(a)
 }
