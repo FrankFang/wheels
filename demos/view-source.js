5b70211be733dcc68ce48dd108ec7ca191331c78
frankfang <frankfang1990@gmail.com>
2017-05-07 23:22:48 +0800

    add view-source buttons

---
+++ demos/view-source.js
@@ -0,0 +1,13 @@
+{
+  let parts = location.pathname.split(/\/|\./)
+  let name = parts[parts.length - 1 - 1]
+  let a = dom.create(`<a id="viewSource" href="https://github.com/FrankFang/wheels#如何阅读源代码">查看源码</a>`)
+  a.style.position = 'fixed'
+  a.style.top = '.2em'
+  a.style.right = '.2em'
+  a.style.background = 'black'
+  a.style.border = '1px solid white'
+  a.style.color = 'white'
+  a.style.padding = '.2em'
+  document.body.appendChild(a)
+}
